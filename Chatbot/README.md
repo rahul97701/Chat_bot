236187c54a2552591dbcc4a6a774a99858e8be20
rahul97701 <singh.rahulsingh.om07@gmail.com>
2024-09-25 14:08:40 -0400

    "chatbot basic project"

---
+++ Chatbot/README.md
@@ -0,0 +1,5 @@
+# Nodejs-Real-time-Chat-App
+Basic real time chat application with;
+* Node.js 
+* socket.io
+* ExpressJS
---
+++ Chatbot/index.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 3700;
+
+// Set View of '/' end point
+app.set('views', __dirname + '/views');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').express);
+app.get("/", function(req, res) {
+    res.render("page");
+});
+
+// use our public/chat.js file as listener
+app.use(express.static(__dirname + '/public'));
+
+// set PORT
+
+var midPort = app.listen(port, function() {
+    console.log("Node.js listening on port" + port);
+})
+
+var io = require('socket.io').listen(midPort);
+
+// set up socket connection
+
+io.sockets.on('connection', function(socket) {
+    socket.emit('message', { message: 'Welcome to the Real Time Web Chat'});
+    socket.on('send', function(data) {
+        io.sockets.emit('message', data);
+    });
+});
